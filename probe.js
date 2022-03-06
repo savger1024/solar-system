a652764a1405bc212f1ea3a1f9a76f3222d8ab8b
savger1024 <90380477+savger1024@users.noreply.github.com>
2022-03-06 15:42:46 +0100

    Create probe.js
    Hogy működjön az overlay

---
+++ probe.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function on() {
+  document.getElementById("window").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("planetEarth").style.display = "block";
+}
+
+function off() {
+  document.getElementById("window").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("planetEarth").style.display = "none";
+}
