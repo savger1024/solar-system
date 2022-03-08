ed89d63eb2905aea51b243d81000864494dcc55d
savger1024 <90380477+savger1024@users.noreply.github.com>
2022-03-08 11:47:06 +0100

    Maybe more line of code than should be -> js
    It "deselects" the previous planet and selects the planet clicked

--- probe.js
+++ probe.js
@@ -1,13 +1,111 @@
 "use strict";
 
-function on() {
+function on1() {
   document.getElementById("window").style.display = "block";
   document.getElementById("overlay").style.display = "block";
-  document.getElementById("planetEarth").style.display = "block";
+  document.getElementById("planet1").style.display = "block";
+  document.getElementById("planet2").style.display = "none";
+  document.getElementById("planet3").style.display = "none";
+  document.getElementById("planet4").style.display = "none";
+  document.getElementById("planet5").style.display = "none";
+  document.getElementById("planet6").style.display = "none";
+  document.getElementById("planet7").style.display = "none";
+  document.getElementById("planet8").style.display = "none";
+}
+function on2() {
+  document.getElementById("window").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("planet1").style.display = "none";
+  document.getElementById("planet2").style.display = "block";
+  document.getElementById("planet3").style.display = "none";
+  document.getElementById("planet4").style.display = "none";
+  document.getElementById("planet5").style.display = "none";
+  document.getElementById("planet6").style.display = "none";
+  document.getElementById("planet7").style.display = "none";
+  document.getElementById("planet8").style.display = "none";
+}
+function on3() {
+  document.getElementById("window").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("planet1").style.display = "none";
+  document.getElementById("planet2").style.display = "none";
+  document.getElementById("planet3").style.display = "block";
+  document.getElementById("planet4").style.display = "none";
+  document.getElementById("planet5").style.display = "none";
+  document.getElementById("planet6").style.display = "none";
+  document.getElementById("planet7").style.display = "none";
+  document.getElementById("planet8").style.display = "none";
+}
+function on4() {
+  document.getElementById("window").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("planet1").style.display = "none";
+  document.getElementById("planet2").style.display = "none";
+  document.getElementById("planet3").style.display = "none";
+  document.getElementById("planet4").style.display = "block";
+  document.getElementById("planet5").style.display = "none";
+  document.getElementById("planet6").style.display = "none";
+  document.getElementById("planet7").style.display = "none";
+  document.getElementById("planet8").style.display = "none";
+}
+function on5() {
+  document.getElementById("window").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("planet1").style.display = "none";
+  document.getElementById("planet2").style.display = "none";
+  document.getElementById("planet3").style.display = "none";
+  document.getElementById("planet4").style.display = "none";
+  document.getElementById("planet5").style.display = "block";
+  document.getElementById("planet6").style.display = "none";
+  document.getElementById("planet7").style.display = "none";
+  document.getElementById("planet8").style.display = "none";
+}
+function on6() {
+  document.getElementById("window").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("planet1").style.display = "none";
+  document.getElementById("planet2").style.display = "none";
+  document.getElementById("planet3").style.display = "none";
+  document.getElementById("planet4").style.display = "none";
+  document.getElementById("planet5").style.display = "none";
+  document.getElementById("planet6").style.display = "block";
+  document.getElementById("planet7").style.display = "none";
+  document.getElementById("planet8").style.display = "none";
+}
+function on7() {
+  document.getElementById("window").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("planet1").style.display = "none";
+  document.getElementById("planet2").style.display = "none";
+  document.getElementById("planet3").style.display = "none";
+  document.getElementById("planet4").style.display = "none";
+  document.getElementById("planet5").style.display = "none";
+  document.getElementById("planet6").style.display = "none";
+  document.getElementById("planet7").style.display = "block";
+  document.getElementById("planet8").style.display = "none";
+}
+function on8() {
+  document.getElementById("window").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("planet1").style.display = "none";
+  document.getElementById("planet2").style.display = "none";
+  document.getElementById("planet3").style.display = "none";
+  document.getElementById("planet4").style.display = "none";
+  document.getElementById("planet5").style.display = "none";
+  document.getElementById("planet6").style.display = "none";
+  document.getElementById("planet7").style.display = "none";
+  document.getElementById("planet8").style.display = "block";
 }
 
 function off() {
   document.getElementById("window").style.display = "none";
   document.getElementById("overlay").style.display = "none";
-  document.getElementById("planetEarth").style.display = "none";
+  document.getElementById("planet1").style.display = "none";
+  document.getElementById("planet2").style.display = "none";
+  document.getElementById("planet3").style.display = "none";
+  document.getElementById("planet4").style.display = "none";
+  document.getElementById("planet5").style.display = "none";
+  document.getElementById("planet6").style.display = "none";
+  document.getElementById("planet7").style.display = "none";
+  document.getElementById("planet8").style.display = "none";
 }
